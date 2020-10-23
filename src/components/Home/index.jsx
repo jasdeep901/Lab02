fe2b7a1ff19916d2d3d12e39bfbf80d25dbfd4ce
Jasdeep901 <jasdeepkaurbhure@gmail.com>
2020-10-23 01:01:18 -0400

    .

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from '../shared/Header';
 import {Link} from 'react-router-dom';
 
-// You need to import your shared header...
+ 
 
 const Home = () => {
   return (
