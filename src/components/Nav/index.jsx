312d5fbfd790a909298484ef9294b00261643b2c
Jasdeep901 <jasdeepkaurbhure@gmail.com>
2020-10-23 01:00:02 -0400

    -intit

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -18,7 +18,7 @@ const Nav = () => {
       <Styles.Nav>
         <ul>
           <li>
-          <Link to="/">Home</Link>
+          <Link to="/Home">Home</Link>
           </li>
           <li>
           <Link to="/About">About</Link>
@@ -27,7 +27,7 @@ const Nav = () => {
       </Styles.Nav>
 
       <Switch>
-      <Route exact path = "/">
+      <Route exact path = "/Home">
            <Home/>
          </Route>
          <Route exact path = "/About">
